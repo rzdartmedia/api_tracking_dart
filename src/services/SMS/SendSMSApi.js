3861afc6fac607d7dc7aac8dfd5f1da31fbac87c
rzdartmedia <rizatdartmedia@gmail.com>
2023-03-15 14:34:11 +0700

    update fixing API SMS

--- src/services/SMS/SendSMSApi.js
+++ src/services/SMS/SendSMSApi.js
@@ -1,28 +1,35 @@
 const axios = require('axios');
+const FormData = require('form-data');
+const { nanoid } = require('nanoid')
 
 async function SendSMSApi({ number, message }) {
     number = number.replace(/^0/, "62");
-    const data = {
-        uid: process.env.UID,
-        pwd: process.env.PWD,
-        messageId: "afeey32646",
-        sc: process.env.SC,
-        msisdn: number,
-        smstype: 0,
-        sms: message
-    };
+    const messageId = nanoid(10);
+
+    const data = new FormData();
+    data.append('uid', process.env.UID);
+    data.append('pwd', process.env.PASSWORD);
+    data.append('messageId', messageId);
+    data.append('sc', process.env.SC);
+    data.append('msisdn', number);
+    data.append('smstype', '1');
+    data.append('sms', message);
 
     const config = {
         method: 'POST',
         maxBodyLength: Infinity,
         url: process.env.URLSMS,
+        data: data,
         headers: {
-            'Content-Type': 'application/json'
-        },
-        data: data
-    };
+            'Content-Type': 'application/json',
+            ...data.getHeaders()
+        }
+    }
 
     const result = await axios(config);
+    console.log(`UID ${process.env.UID}`)
+    console.log(`PWD ${process.env.PASSWORD}`)
+    console.log(`Send SMS to ${number}`)
     console.log(result.data);
 }
 
